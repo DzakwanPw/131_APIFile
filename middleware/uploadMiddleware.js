8efda8ec6b183454b8c2a0f4149d466b662f0477
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2026-08-10 10:42:18 +0700

    perbaiki kesalahan sintaksis pada fungsi destination dan filename di uploadMiddleware.js

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -2,11 +2,11 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) => {
+    destination: function (req, file, cb) {
         cb(null, 'uploads/');
     },
 
-    filename: function (req, file, cb) => {
+    filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + file.originalname;
         cb(null, uniqueSuffix);
     }
